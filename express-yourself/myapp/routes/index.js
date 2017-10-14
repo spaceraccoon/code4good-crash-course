abfb34ad5aaa66548c269bfa7b9b2729450ad491
Eugene Lim <limzhiweieugene@gmail.com>
2017-10-14 17:47:36 -0400

    Update index.js

--- express-yourself/myapp/routes/index.js
+++ express-yourself/myapp/routes/index.js
@@ -4,14 +4,14 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
   res.render('yich', {name: 'Yi Chern' });
-})
+});
 
 router.get('/home', function(req, res, next) {
   res.render('index', { title: "is this home?" });
 });
 
 router.get('/nadia', function(req, res, next) {
-	res.render('index', {title: 'hi' });
-    });
+  res.render('index', {title: 'hi' });
+});
 
 module.exports = router;
